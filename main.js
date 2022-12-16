7829e0caf5a4a3ded5970622a687b6fa636c493d
thavin8220 <thavinmurugan@gmail.com>
2022-12-16 16:45:26 +0530

    add file

---
+++ main.js
@@ -0,0 +1,9 @@
+let a=(prompt("Enter The name"));
+let b=parseInt(prompt("Enter the number"));
+function myName(a,b){
+for(i=1;i<=b;i++){
+   document.write(a);
+   document.write("<br>")
+}
+}
+myName(a,b)
